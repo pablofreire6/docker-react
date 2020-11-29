5b9792380e2822535f4bf674ffbbb926be802cd6
Pablo Freire <pablofreire6@gmail.com>
2020-11-28 23:50:26 -0300

    fix test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>learn react</p>
       </header>
     </div>
   );
